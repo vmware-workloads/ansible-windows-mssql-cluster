02b7a77c9c2ba0c3639d3abd366dfc5fac3a0628
Dharmesh Bhatt <48768405+darkmesh-b@users.noreply.github.com>
2024-07-25 14:25:23 +0100

    Update db_script

--- db_script.ps1
+++ db_script.ps1
@@ -1,3 +1,16 @@
-# 
-
-write-host "ello world"
+USE master;
+GO
+CREATE DATABASE myDB
+ON
+( NAME = myDB_dat,
+    FILENAME = ‘F:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\DATA\mydb_dat.mdf',
+    SIZE = 10,
+    MAXSIZE = 50,
+    FILEGROWTH = 5 )
+LOG ON
+( NAME = myDB_log,
+    FILENAME = ‘E:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\DATA\mydb_log.ldf',
+    SIZE = 5MB,
+    MAXSIZE = 25MB,
+    FILEGROWTH = 5MB ) ;
+GO
